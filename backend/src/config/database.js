e85a7586b7f6a2983bed202067a99e396fb16110
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-16 11:51:38 -0300

    :sparkles: feat: Criação do banco de dados

---
+++ backend/src/config/database.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+const database = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  }
+);
+
+module.exports = {
+    database,
+};
