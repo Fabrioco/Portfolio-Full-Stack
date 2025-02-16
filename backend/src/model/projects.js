51c35a136a38b3469c5e9eebc3191aad954acaf4
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-16 12:29:26 -0300

    :bug: fix: Correção do banco de dados com exportação e Modelo do projeto

--- backend/src/model/projects.js
+++ backend/src/model/projects.js
@@ -1,29 +1,35 @@
 const { DataTypes } = require("sequelize");
 const database = require("../config/database");
 
-const Project = database.define("projects", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    unique: true,
+const Project = database.define(
+  "projects",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      unique: true,
+    },
+    title: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    time_worked: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.BLOB("long"),
+      allowNull: false,
+    },
   },
-  title: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-  },
-  category: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-  },
-  time_worked: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-  },
-  image: {
-    type: DataTypes.BLOB("long"),
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 module.exports = Project;
