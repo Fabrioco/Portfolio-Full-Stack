4ad318203b8e418d9508bd00e287bd221913eae3
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-17 00:41:27 -0300

    :sparkles: feat: Criei o 'sobre mim' para mobile

--- frontend/src/app/dashboard/aboutMe/page.tsx
+++ frontend/src/app/dashboard/aboutMe/page.tsx
@@ -1,22 +1,15 @@
+import { Description } from "@/app/ui/aboutMe/description";
+import { Title } from "@/app/ui/aboutMe/title";
+
 export default function AboutMe() {
   return (
-    <div>
-      <h1>About Me</h1>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum quam
-        sequi maxime quibusdam debitis obcaecati sed quaerat non sint neque
-        optio dolorem, tenetur tempora? Eveniet quam beatae placeat dolor fugit.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
-        quis laborum fugiat perspiciatis. Voluptatum nesciunt possimus vel, iure
-        cum esse cumque atque dolorem ab commodi dolores eaque, fugiat quasi
-        suscipit! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Exercitationem deleniti itaque delectus atque mollitia nobis, corrupti
-        hic qui officiis quisquam nemo sint. Quas, quaerat ullam alias facilis
-        aliquam eaque in? Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Animi exercitationem esse porro at veritatis cupiditate molestias
-        amet voluptatibus pariatur illum ex odio minus omnis dolores neque,
-        nihil assumenda! Vitae, tempora?
-      </p>
+    <div className="w-11/12 h-auto flex flex-col justify-center items-center text-white px-2 gap-10 mx-auto">
+      <div className="text-center w-10/12 md:w-1/2">
+        <Title />
+      </div>
+      <div className="w-10/12 md:w-1/2  text-justify text-paragraph mb-4">
+        <Description />
+      </div>
     </div>
   );
 }
