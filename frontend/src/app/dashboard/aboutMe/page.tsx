bdfd177280613ffdc43ea3cebdb18799c377e4d8
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-16 22:29:36 -0300

    :sparkles: feat: Criei a página 'Sobre mim'

---
+++ frontend/src/app/dashboard/aboutMe/page.tsx
@@ -0,0 +1,22 @@
+export default function AboutMe() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum quam
+        sequi maxime quibusdam debitis obcaecati sed quaerat non sint neque
+        optio dolorem, tenetur tempora? Eveniet quam beatae placeat dolor fugit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        quis laborum fugiat perspiciatis. Voluptatum nesciunt possimus vel, iure
+        cum esse cumque atque dolorem ab commodi dolores eaque, fugiat quasi
+        suscipit! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Exercitationem deleniti itaque delectus atque mollitia nobis, corrupti
+        hic qui officiis quisquam nemo sint. Quas, quaerat ullam alias facilis
+        aliquam eaque in? Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Animi exercitationem esse porro at veritatis cupiditate molestias
+        amet voluptatibus pariatur illum ex odio minus omnis dolores neque,
+        nihil assumenda! Vitae, tempora?
+      </p>
+    </div>
+  );
+}
