9614c37d0b0ab7b12ccf2aaefa8af7a39ec24cd2
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-17 00:58:29 -0300

    :sparkles: feat: Criei o nav de botões

---
+++ frontend/src/app/ui/learnings/navButtons.tsx
@@ -0,0 +1,10 @@
+export function NavButtons() {
+  return (
+    <nav className="flex gap-2 items-center overflow-x-auto">
+      <button>Front-End</button>
+      <button>Back-end</button>
+      <button>Banco de Dados</button>
+      <button>Outros</button>
+    </nav>
+  );
+}
