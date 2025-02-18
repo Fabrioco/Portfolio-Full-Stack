dd011be96653fe77380c724553210c6112b062e5
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-18 15:32:41 -0300

    :sparkles: feat: Adicionei transição de background nos botoẽs

--- frontend/src/app/ui/learnings/navButtons.tsx
+++ frontend/src/app/ui/learnings/navButtons.tsx
@@ -1,18 +1,24 @@
+"use client";
+import React from "react";
+
 export function NavButtonsLearnings() {
+  const [indexButton, setIndexButton] = React.useState<number>(0);
+
+  const buttonText = ["Front-End", "Back-end", "Banco de Dados", "Outros"];
+
   return (
     <nav className="flex gap-4 items-center overflow-x-auto text-nowrap">
-      <button className="bg-button border border-button text-buttonText px-4 py-2 font-bold rounded-full">
-        Front-End
-      </button>
-      <button className="bg-button border border-button text-buttonText px-4 py-2 font-bold rounded-full">
-        Back-end
-      </button>
-      <button className="bg-button border border-button text-buttonText px-4 py-2 font-bold rounded-full">
-        Banco de Dados
-      </button>
-      <button className="bg-button border border-button text-buttonText px-4 py-2 font-bold rounded-full">
-        Outros
-      </button>
+      {buttonText.map((text, index) => (
+        <button
+          key={index}
+          onClick={() => setIndexButton(index)}
+          className={`bg-button border border-button text-buttonText px-4 py-2 font-bold rounded-full transition-all duration-50 active:scale-95 shadow-button shadow-inner
+            ${indexButton === index && "bg-opacity-45"}
+          `}
+        >
+          {text}
+        </button>
+      ))}
     </nav>
   );
 }
