09515a277093fe81601cb1968107ab9b1e6ac117
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-18 13:28:42 -0300

    :lipstick: fix: Estilização dos botões

--- frontend/src/app/ui/learnings/navButtons.tsx
+++ frontend/src/app/ui/learnings/navButtons.tsx
@@ -1,10 +1,18 @@
 export function NavButtonsLearnings() {
   return (
-    <nav className="flex gap-2 items-center overflow-x-auto">
-      <button>Front-End</button>
-      <button>Back-end</button>
-      <button>Banco de Dados</button>
-      <button>Outros</button>
+    <nav className="flex gap-4 items-center overflow-x-auto text-nowrap">
+      <button className="bg-button border border-button text-buttonText px-4 py-2 font-bold rounded-full">
+        Front-End
+      </button>
+      <button className="bg-button border border-button text-buttonText px-4 py-2 font-bold rounded-full">
+        Back-end
+      </button>
+      <button className="bg-button border border-button text-buttonText px-4 py-2 font-bold rounded-full">
+        Banco de Dados
+      </button>
+      <button className="bg-button border border-button text-buttonText px-4 py-2 font-bold rounded-full">
+        Outros
+      </button>
     </nav>
   );
 }
