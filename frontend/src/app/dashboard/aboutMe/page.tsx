b373aa975b25dd894545e7ef25481eba2e798f7d
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-18 18:46:04 -0300

    :book: docs: Coloquei as divs junta com o componente para reduzir o tamanho na pagina About

--- frontend/src/app/dashboard/aboutMe/page.tsx
+++ frontend/src/app/dashboard/aboutMe/page.tsx
@@ -11,12 +11,8 @@ export default function AboutMe() {
   return (
     <div className="w-11/12 h-auto flex flex-col md:flex-row justify-center items-center text-white px-2 gap-10 mx-auto">
       <div className="flex flex-col items-center gap-10">
-        <div className="text-center w-10/12 md:w-1/2">
-          <TitleAboutMe />
-        </div>
-        <div className="w-10/12 lg:w-1/2  text-justify text-paragraph mb-4">
-          <DescriptionAboutMe />
-        </div>
+        <TitleAboutMe />
+        <DescriptionAboutMe />
       </div>
       <div className="hidden w-11/12 md:w-1/2 lg:flex flex-col justify-center gap-10">
         <TitleLearnings />
