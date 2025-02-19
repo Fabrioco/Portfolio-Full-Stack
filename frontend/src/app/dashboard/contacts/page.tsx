23e878b74fd996acb21c40304459109a431bc332
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-19 15:05:54 -0300

    :sparkles: feat: Pagina de contatos

---
+++ frontend/src/app/dashboard/contacts/page.tsx
@@ -0,0 +1,13 @@
+import { Contact } from "@/ui/contacts/contacts";
+import { SocialMedia } from "@/ui/contacts/socialMedia";
+import { TitleContacts } from "@/ui/contacts/title";
+
+export default function Contacts() {
+  return (
+    <div className="w-11/12 h-full flex flex-col gap-10 mx-auto justify-center md:items-center text-white md:text-center">
+      <TitleContacts />
+      <SocialMedia />
+      <Contact />
+    </div>
+  );
+}
