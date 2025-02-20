f408a6553f9a7596836af2a6f1ca9b004fed461c
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-20 16:01:04 -0300

    :sparkles: feat: Adicionei o componente de seleção da categoria para o mobile

--- frontend/src/app/dashboard/learnings/page.tsx
+++ frontend/src/app/dashboard/learnings/page.tsx
@@ -1,19 +1,11 @@
-import { GridBackEndLearnings } from "@/ui/learnings/gridBackEnd";
-import { GridDatabaseLearnings } from "@/ui/learnings/gridDatabase";
-import { GridFrontEndLearnings } from "@/ui/learnings/gridFrontEnd";
-import { GridOthersLearnings } from "@/ui/learnings/gridOthers";
-import { NavButtonsLearnings } from "@/ui/learnings/navButtons";
+import { SelectionCategory } from "@/ui/learnings/selectionCategory";
 import { TitleLearnings } from "@/ui/learnings/title";
 
 export default function Learnings() {
   return (
     <div className="w-11/12 md:w-1/2 flex flex-col justify-center mx-auto gap-10 md:text-center lg:hidden">
       <TitleLearnings />
-      <NavButtonsLearnings />
-      <GridFrontEndLearnings />
-      {/* <GridBackEndLearnings />
-      <GridDatabaseLearnings />
-      <GridOthersLearnings /> */}
+      <SelectionCategory />
     </div>
   );
 }
