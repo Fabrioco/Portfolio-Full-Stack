335660d19dc5bd55926216d4254799a3a4481702
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-20 16:06:20 -0300

    :sparkles: feat: Fiz o redirecionamento para a pagina sobre mim caso o usuário esteja vendo pelo desktop

--- frontend/src/app/dashboard/learnings/page.tsx
+++ frontend/src/app/dashboard/learnings/page.tsx
@@ -1,7 +1,19 @@
+"use client";
 import { SelectionCategory } from "@/ui/learnings/selectionCategory";
 import { TitleLearnings } from "@/ui/learnings/title";
+import { redirect } from "next/navigation";
+import React from "react";
 
 export default function Learnings() {
+  const [isClient, setIsClient] = React.useState<boolean>(false);
+  React.useEffect(() => {
+    setIsClient(true);
+
+    if (window.innerWidth > 1024) return redirect("/dashboard/aboutMe");
+  }, []);
+
+  if (!isClient) return null;
+
   return (
     <div className="w-11/12 md:w-1/2 flex flex-col justify-center mx-auto gap-10 md:text-center lg:hidden">
       <TitleLearnings />
