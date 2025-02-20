20666f12540736eee80721ff61b8256aaf1ac417
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-20 17:26:43 -0300

    :sparkles: feat: Fiz uma função para buscar todos os projetos no banco de dados

--- frontend/src/ui/projects/projectsList.tsx
+++ frontend/src/ui/projects/projectsList.tsx
@@ -1,19 +1,51 @@
 "use client";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Project } from "./project";
 
+interface ProjectData {
+  id: number;
+  title: string;
+  category: string;
+  time_worked: string;
+  image: string;
+  link: string;
+}
+
 export function ProjectsList() {
+  const [projects, setProjects] = useState<ProjectData[]>([]);
+
+  useEffect(() => {
+    async function fetchProjects() {
+      try {
+        const response = await fetch("http://localhost:5000/api/projects");
+
+        if (!response.ok) throw new Error("Erro ao buscar projetos");
+
+        const data: ProjectData[] = await response.json();
+        setProjects(data);
+      } catch (error) {
+        console.error("Erro ao buscar projetos:", error);
+      } finally {
+      }
+    }
+
+    fetchProjects();
+  }, []);
+
+  console.log(projects);
+
   return (
     <div className="w-full overflow-x-auto flex flex-row md:gap-10 gap-5 items-center">
-      {[1, 2, 3, 4, 5].map((item, index) => (
+      {projects.map((project, index) => (
         <motion.div
-          key={index}
+          key={project.id}
           initial={{ opacity: 0, scale: 0, zIndex: 0 }}
           animate={{ opacity: 1, scale: 1, zIndex: 1 }}
           exit={{ opacity: 0, scale: 0, zIndex: 0 }}
           transition={{ duration: 0.5, delay: index * 0.5 }}
         >
-          <Project />
+          <Project project={project} />
         </motion.div>
       ))}
     </div>
