f87556f3b29a2d3db150a540892520c7dd5b110a
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-20 16:21:05 -0300

    :lipstick: fix: Corrigi os espaçamentos do footer

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -7,9 +7,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full relative">
       <Header />
-      <div className="w-11/12 mx-auto mb-20">{children}</div>
+      <div className="w-11/12 h-full mx-auto mb-5">{children}</div>
       <Footer />
     </div>
   );
