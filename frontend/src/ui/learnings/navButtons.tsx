7f58e22120a5bce7a40d692cbfe38a143906aa39
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-20 15:56:16 -0300

    :sparkles: feat: Crie um componente para mostrar a navbuttons com o grid, de acordo com o botão selecionado

--- frontend/src/ui/learnings/navButtons.tsx
+++ frontend/src/ui/learnings/navButtons.tsx
@@ -2,24 +2,30 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-export function NavButtonsLearnings() {
-  const [indexButton, setIndexButton] = React.useState<number>(0);
+const buttonText = ["Front-End", "Back-end", "Banco de Dados", "Outros"];
 
-  const buttonText = ["Front-End", "Back-end", "Banco de Dados", "Outros"];
+interface NavButtonsLearningsProps {
+  onSelect: (text: string) => void;
+}
+
+export function NavButtonsLearnings({ onSelect }: NavButtonsLearningsProps) {
+  const [indexButton, setIndexButton] = React.useState<number>(0);
 
   return (
     <nav className="flex gap-4 items-center overflow-x-auto text-nowrap">
       {buttonText.map((text, index) => (
         <motion.button
           key={index}
-          onClick={() => setIndexButton(index)}
+          onClick={() => {
+            setIndexButton(index);
+            onSelect(text);
+          }}
           className={`bg-button border border-button text-buttonText px-4 py-2 font-bold rounded-full transition-all duration-50 active:scale-95 shadow-button shadow-inner
-            ${indexButton === index && "bg-opacity-45"}
+            ${indexButton === index ? "bg-opacity-45" : ""}
           `}
           initial={{ opacity: 0, x: -100, scale: 0 }}
           animate={{ opacity: 1, x: 0, scale: 1 }}
-          exit={{ opacity: 0, x: -100, scale: 0 }}
-          transition={{ duration: 0.5, delay: index * 0.5 }}
+          transition={{ duration: 0.5, delay: index * 0.2 }} // Reduzi o delay para evitar animação lenta
         >
           {text}
         </motion.button>
