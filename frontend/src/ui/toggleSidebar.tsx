9ba4457488145178b8062cc826130f465beef1a9
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-21 21:05:19 -0300

    :bug: fix: Corrigi o valor inicial do width

--- frontend/src/ui/toggleSidebar.tsx
+++ frontend/src/ui/toggleSidebar.tsx
@@ -5,11 +5,13 @@ import { useEffect, useState } from "react";
 
 export const ToggleSidebar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [width, setWidth] = useState<number>(window.innerWidth);
+  const [width, setWidth] = useState<number>(0);
 
   useEffect(() => {
+    setWidth(window.innerWidth);
     const handleResize = () => setWidth(window.innerWidth);
     window.addEventListener("resize", handleResize);
+
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
