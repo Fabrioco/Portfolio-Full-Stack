9e11c24830bf714cd08c6c6b789e129b00e306e8
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-22 01:33:15 -0300

    Correção da url da api

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(Express.json({ limit: "10mb" }));
 app.use(
   cors({
-    origin: "https://portfolio-full-stack-yj8l.vercel.app/",
+    origin: "https://portfolio-full-stack-yj8l.vercel.app/dashboard",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
