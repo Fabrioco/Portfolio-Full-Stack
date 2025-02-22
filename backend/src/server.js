f29fe7348cb20a17c2cc8ffc865d2466cc6a93a4
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-22 01:39:51 -0300

    apenas um teste sem cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,13 +8,7 @@ const app = Express();
 const PORT = process.env.PORT || 5000;
 
 app.use(Express.json({ limit: "10mb" }));
-app.use(
-  cors({
-    origin: "https://portfolio-full-stack-yj8l.vercel.app/dashboard",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 
 app.get("/api/projects", async (req, res) => {
   try {
