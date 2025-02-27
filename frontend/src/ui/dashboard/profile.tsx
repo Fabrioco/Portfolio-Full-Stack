5d30c2ba9ffe6d30dd0a8dbb18c764eac6002bef
Fabricio Oliveira Lopes <Fabriciooliveiralopes50@gmail.com>
2025-02-27 12:11:10 -0300

    :book: docs: Mudei a foto do profile

--- frontend/src/ui/dashboard/profile.tsx
+++ frontend/src/ui/dashboard/profile.tsx
@@ -11,7 +11,7 @@ export function Profile() {
       exit={{ opacity: 0, scale: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <p className="absolute -top-3 -right-5 bg-main text-black px-4 py-2 rounded-md rounded-bl-none w-fit font-bold flex items-center gap-2">
+      <p className="absolute -top-3 -right-5 bg-main text-black px-4 py-2 rounded-md rounded-bl-none w-fit font-bold flex items-center gap-2 z-10">
         Oi, eu sou Fabrício
         <i>
           <Hand size={24} weight="fill" className="rotate-45" color="#E4C39F" />
@@ -19,11 +19,11 @@ export function Profile() {
         <span className="absolute w-4 h-4 bg-main rotate-45 -bottom-2 left-1"></span>
       </p>
       <Image
-        src="/profile.jpeg"
+        src="/images/profile.jpg"
         alt="Vercel Logo"
         width={500}
         height={500}
-        className="w-full h-full rounded-full"
+        className="w-full h-full rounded-full rotate-[35deg] object-cover"
       />
     </motion.div>
   );
